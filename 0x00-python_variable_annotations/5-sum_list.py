ade507aa21cfa8509f989b25b5dc17952a91bb13
Otonda1 <charlesotonda@gmail.com>
2024-01-11 05:22:12 -0800

    return a float sum of list elements

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a type annoted function
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns float sum"""
+    return sum(input_list)
