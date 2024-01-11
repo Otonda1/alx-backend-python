bbafe15d2320c47b952da9000f2dcddfd31f3667
Otonda1 <charlesotonda@gmail.com>
2024-01-11 04:45:02 -0800

    annotates variables with default values

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """
-annotates variables with specified values
+Defining type annotated variables
 """
 
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = 'Holberton'
-
+school: str = "Holberton"
